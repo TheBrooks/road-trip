2f0f4e9f5755e342d9d4764d3f2dae5a4a6f4ea8
Ryan Brooks <rbrooks@Ryans-MacBook-Pro.local>
2017-12-28 13:22:06 -0700

    first pass at wireframe

--- src/App.js
+++ src/App.js
@@ -5,12 +5,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title"> Road Trip</h1>
+        <div className="App-destination-input">
+          <form>
+            <input type="text" name="destination" placeHolder="Enter a destination"/ >
+          </form>
+        </div>
+        <div className="rowC">
+          <div className="App-destinations-list">
+            <ul>
+              <li>Austin, TX</li>
+              <li>El Paso,TX</li>
+              <li>Salt Lake City, UT</li>
+              <li>Yosemite National Park, CA</li>
+              <li>Fort Bragg, CA</li>
+              <li>Crater Lake, OR</li>
+              <li>Bend, OR</li>
+              <li>Squamish, CAN</li>
+              <li>Index, WA</li>
+              <li>Bend, OR</li>
+              <li>Portland, OR</li>
+              <li>Yosemite National Park, CA</li>
+              <li>Bishop, CA</li>
+              <li>Las Vegas, NV</li>
+              <li>Zion National Park, UT</li>
+              <li>St. George, UT</li>
+              <li>Bear Ears National Monumant, UT</li>
+              <li>Denver, CO</li>
+              <li>Moab, UT</li>
+              <li>Zion National Park, UT</li>
+              <li>El Paso, TX</li>
+              <li>Austin,TX</li>
+            </ul>
+          </div>
+          <div className="App-map">
+            <div className="App-actual-map">
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
