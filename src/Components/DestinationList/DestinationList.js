f98d6bb13711bd5e09616c04f967a9a4a378d3e3
Ryan Brooks <rbrooks@Ryans-MacBook-Pro.local>
2017-12-28 15:14:19 -0700

    replace hard coded list items in html with hard coded array in object's state

--- src/Components/DestinationList/DestinationList.js
+++ src/Components/DestinationList/DestinationList.js
@@ -2,32 +2,27 @@ import React from 'react';
 import './DestinationList.css';
 
 export default class DestinationList extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			destinations: ["Austin, TX",
+	        "El Paso,TX",
+	        "Salt Lake City, UT",
+	        "Yosemite National Park, CA",
+	        "Fort Bragg, CA",
+	        "Crater Lake, OR",
+	        "Bend, OR",]
+		};
+	}
+
 	render() {
+		const destinationsListItems = this.state.destinations.map((dest) => <li>{dest}</li>);
+
 		return (
 			<div className="DestinationList">
 	      <ul>
-	        <li>Austin, TX</li>
-	        <li>El Paso,TX</li>
-	        <li>Salt Lake City, UT</li>
-	        <li>Yosemite National Park, CA</li>
-	        <li>Fort Bragg, CA</li>
-	        <li>Crater Lake, OR</li>
-	        <li>Bend, OR</li>
-	        <li>Squamish, CAN</li>
-	        <li>Index, WA</li>
-	        <li>Bend, OR</li>
-	        <li>Portland, OR</li>
-	        <li>Yosemite National Park, CA</li>
-	        <li>Bishop, CA</li>
-	        <li>Las Vegas, NV</li>
-	        <li>Zion National Park, UT</li>
-	        <li>St. George, UT</li>
-	        <li>Bear Ears National Monumant, UT</li>
-	        <li>Denver, CO</li>
-	        <li>Moab, UT</li>
-	        <li>Zion National Park, UT</li>
-	        <li>El Paso, TX</li>
-	        <li>Austin,TX</li>
+	      	{destinationsListItems}
 	      </ul>
 	    </div>
 		);
