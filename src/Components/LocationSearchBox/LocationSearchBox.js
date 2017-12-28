bb0170984164a3747fadc97a3e4156b6a4bf1c44
Ryan Brooks <rbrooks@Ryans-MacBook-Pro.local>
2017-12-28 15:06:04 -0700

    set up the location search box to handle a form submit

--- src/Components/LocationSearchBox/LocationSearchBox.js
+++ src/Components/LocationSearchBox/LocationSearchBox.js
@@ -2,11 +2,31 @@ import React from 'react';
 import './LocationSearchBox.css';
 
 export default class LocationSearchBox extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: ''
+		};
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+
+	handleSubmit(event) {
+		alert('a name was submitted: ' + this.state.value);
+		event.preventDefault();
+		this.setState({value: ''});
+	}
+
 	render() {
 		return( 
 			<div className="LocationSearchBox">
-      	<form>
-        	<input type="text" name="destination" placeHolder="Enter a destination"/ >
+      	<form onSubmit={this.handleSubmit}>
+        	<input type="text" value={this.state.value} onChange={this.handleChange} placeHolder="Enter a destination"/ >
     	  </form>
     	</div>
     );
