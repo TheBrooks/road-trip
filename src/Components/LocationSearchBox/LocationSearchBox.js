c3978f0c4c182cc076cdea3b9c7cff69a2924194
Ryan Brooks <rbrooks@Ryans-MacBook-Pro.local>
2017-12-29 14:53:08 -0700

    inserted and styled the react-places-autocomplete search bar

--- src/Components/LocationSearchBox/LocationSearchBox.js
+++ src/Components/LocationSearchBox/LocationSearchBox.js
@@ -1,36 +1,54 @@
 import React from 'react';
 import './LocationSearchBox.css';
+import PlacesAutocomplete /*, {geocodeByAddress, getLatLng }*/ from 'react-places-autocomplete';
 
 export default class LocationSearchBox extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value: ''
+			address: ''
 		};
 
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleEnter = this.handleEnter.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 	}
 
-	handleChange(event) {
-		this.setState({value: event.target.value});
+	handleChange(address) {
+		this.setState({address: address});
+	}
+
+	handleEnter(address) {
+		/*
+		geocodeByAddress(this.state.address)
+			.then(results => getLatLng(results[0]))
+			.then(latLng => console.log('Success', latLng))
+			.catch(error => console.error('Error', error))
+		*/
+		this.props.onSubmit(this.state.address);
+		this.setState({address: ''});
 	}
 
 	handleSubmit(event) {
 		event.preventDefault();
-		this.props.onSubmit(this.state.value);
-		this.setState({value: ''});
+		this.handleEnter(this.state.address);
 	}
 
 	render() {
+		const inputProps = {
+			value: this.state.address,
+			onChange: this.handleChange,
+			placeholder: 'Enter a destination'
+		}
+
+		const myStyles = {
+			input: {boxSizing: 'border-box', borderRadius: '99px'},
+		}
+
 		return( 
 			<div className="LocationSearchBox">
       	<form onSubmit={this.handleSubmit}>
-        	<input 
-        		type="text" 
-        		value={this.state.value} 
-        		onChange={this.handleChange} 
-        		placeholder="Enter a destination"/ >
+      		<PlacesAutocomplete styles={myStyles} className="PlacesAutocomplete" inputProps={inputProps} onEnterKeyDown={this.handleEnter}/>
     	  </form>
     	</div>
     );
