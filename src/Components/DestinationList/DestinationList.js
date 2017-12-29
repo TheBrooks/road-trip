c2a97723eecf129cd1c22a1cc729241cb766ed60
Ryan Brooks <rbrooks@Ryans-MacBook-Pro.local>
2017-12-29 11:55:55 -0700

    removed unused constructor

--- src/Components/DestinationList/DestinationList.js
+++ src/Components/DestinationList/DestinationList.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './DestinationList.css';
 
 export default class DestinationList extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		const destinationsListItems = this.props.destinations.map((dest) => <li>{dest}</li>);
 
